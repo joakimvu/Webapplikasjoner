5b5a3ac4d0aba6c36f1ef3270f8f6fd7766f702a
joakimvu <joakim.vu@hiof.no>
2022-08-23 19:08:52 +0200

    Filtrert liste
    Ny array som lagrer filtrert liste

--- Intro Javascript/Oppgave 2/src/start.js
+++ Intro Javascript/Oppgave 2/src/start.js
@@ -37,22 +37,33 @@ users.forEach(
   (user) =>
     (userList.innerHTML += `<li><span>${user.id}</span><span>${user.name}</span><span>${user.age}</span></li>`)
 );
+
+// Liste med filtrerte brukere
+const filteredUsers = [];
+
 // TODO: Lag en funksjon som håndterer søket og oppdaterer grensesnittet med resultatet fra søket
 const inputName = document.getElementById("name");
 
 const searchName = (e) => {
+  // Tømmer lister
   userList.innerHTML = null;
   userList.innerHTML += `<li><h4>Id</h4><h4>Name</h4><h4>Age</h4></li>`;
-
+  filteredUsers.length = 0;
   // Skriver ut ny liste basert på søk etter navn
   users.forEach((user) => {
     if (user.name.toLowerCase().includes(inputName.value.toLowerCase())) {
+      // if (Object.keys(filteredUsers) !== user.id) {
+      //   filteredUsers.push({ id: user.id, name: user.name, age: user.age });
+      // }
+      filteredUsers.push({ id: user.id, name: user.name, age: user.age });
       userList.innerHTML += `<li><span>${user.id}</span><span>${user.name}</span><span>${user.age}</span></li>`;
     }
   });
-  console.log(filteredUsers);
+  // console.log(filteredUsers);
 };
 // TODO: Lag en funksjon som håndterer filteret og oppdaterer grensesnittet med resultatet fra filteret
+const handleFilter = () => {};
 // TODO: Lytt til tastatur klikk på søkefeltet, den skal trigge søkefunksjonen (handleSearch)
 inputName.addEventListener("keyup", searchName);
 // TODO: Lytt til klikk på filter-knappen, den skal trigge filterfunksjonen (handleFilter)
+const filterBtn = document.getElementById("filter");
