65e2aff9ed99b48e397f65f8eda3f9c54225cba3
joakimvu <joakim.vu@hiof.no>
2022-08-23 18:42:49 +0200

    Filter navn
    Filtrer på navn virker, både med store og små bokstaver.

--- Intro Javascript/Oppgave 2/src/start.js
+++ Intro Javascript/Oppgave 2/src/start.js
@@ -27,11 +27,32 @@ const createUsers = (userCount) => {
     users.push(user);
   }
 };
-createUsers(11);
+createUsers(10);
 
 // TODO: Hent HTML #id med getElementById
+const userList = document.getElementById("users");
 // TODO: Lag en funksjon som kan brukes til å skrive ut HTMLen for å se brukerene. Du kan bruke users importert over for å hente en liste med 10 brukere
+userList.innerHTML += `<li><h4>Id</h4><h4>Name</h4><h4>Age</h4></li>`;
+users.forEach(
+  (user) =>
+    (userList.innerHTML += `<li><span>${user.id}</span><span>${user.name}</span><span>${user.age}</span></li>`)
+);
 // TODO: Lag en funksjon som håndterer søket og oppdaterer grensesnittet med resultatet fra søket
+const inputName = document.getElementById("name");
+
+const searchName = (e) => {
+  userList.innerHTML = null;
+  userList.innerHTML += `<li><h4>Id</h4><h4>Name</h4><h4>Age</h4></li>`;
+
+  // Skriver ut ny liste basert på søk etter navn
+  users.forEach((user) => {
+    if (user.name.toLowerCase().includes(inputName.value.toLowerCase())) {
+      userList.innerHTML += `<li><span>${user.id}</span><span>${user.name}</span><span>${user.age}</span></li>`;
+    }
+  });
+  console.log(filteredUsers);
+};
 // TODO: Lag en funksjon som håndterer filteret og oppdaterer grensesnittet med resultatet fra filteret
 // TODO: Lytt til tastatur klikk på søkefeltet, den skal trigge søkefunksjonen (handleSearch)
+inputName.addEventListener("keyup", searchName);
 // TODO: Lytt til klikk på filter-knappen, den skal trigge filterfunksjonen (handleFilter)
