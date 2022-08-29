b39cd83dbd6114df9efd69f59b6a44e676cfcff4
joakimvu <joakim.vu@hiof.no>
2022-08-29 13:55:07 +0200

    update

--- forelesninger/sort_words/main.js
+++ forelesninger/sort_words/main.js
@@ -25,3 +25,20 @@ const wordList = [
   "Ake",
   "Dra",
 ];
+
+// TODO Hente ut alle elementer
+const spans = document.querySelectorAll("span")
+const inputs = document.querySelectorAll("input")
+const testButtonId = document.querySelector("#test")
+
+
+const random = () => {
+  return Math.floor(Math.random()*wordList.length)
+}
+
+const addWord = () => {
+  [...spans].forEach((span) => (span.innerHTML = wordList[random()]))
+  // Array.from(spans).forEach((span) => (span.innerHTML = wordList[random()]))
+}
+
+addWord()
