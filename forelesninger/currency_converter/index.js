68c5962665b1fc1e4895b3e0e62ca7ee3b148eb8
joakimvu <joakim.vu@hiof.no>
2022-08-29 09:23:14 +0200

    Update index.js

--- forelesninger/currency_converter/index.js
+++ forelesninger/currency_converter/index.js
@@ -1,10 +1,6 @@
-import {add} from './math.js'
-
-const buttonId = document.getElementById('add');
-
-buttonId?.addEventListener('click', function() {
-  const valueOne = Math.floor(Math.random() * 10);
-  const valueTwo = Math.floor(Math.random() * 10);
-  add(valueOne, valueTwo);
-})
-
+ 
+const prompt = require('prompt-sync')();
+//
+// get input from the user.
+//
+const n = prompt('How many more times? ');
