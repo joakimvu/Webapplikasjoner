89a68b9cff57e595020776b466f9e39e950fa5b8
joakimvu <joakim.vu@hiof.no>
2022-09-05 09:01:44 +0200

    Initial commit

--- forelesninger/typescript-intro/1-types/src/01-primitive-types.ts
+++
@@ -1,9 +0,0 @@
-export const stringConstant: string = 'Hey'
-
-export const numberConstant = 42
-
-export const booleanConstant = false
-
-export const nullConstant = null
-
-export const undefinedConstant = undefined
