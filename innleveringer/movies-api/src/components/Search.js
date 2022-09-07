a563eacb9d048fdf2467dacb43658450718c739b
joakimvu <joakim.vu@hiof.no>
2022-09-07 14:44:31 +0200

    Fixed form

--- innleveringer/movies-api/src/components/Search.js
+++ innleveringer/movies-api/src/components/Search.js
@@ -1,11 +1,9 @@
 const Search = ({ search, setSeach, handleInput, handleSearch }) => {
   return (
-    <section className="search-section">
+    <form onSubmit={handleSearch}>
       <input onChange={handleInput} type="text" placeholder="Søk på filmer" />
-      <button onClick={handleSearch} type="button">
-        Søk
-      </button>
-    </section>
+      <button type="submit">Søk</button>
+    </form>
   );
 };
 
