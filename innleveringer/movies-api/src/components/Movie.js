a4e1c8c501eaa106473acd520eb9da2e0f775df0
joakimvu <joakim.vu@hiof.no>
2022-09-07 14:48:02 +0200

    Update Movie.js

--- innleveringer/movies-api/src/components/Movie.js
+++ innleveringer/movies-api/src/components/Movie.js
@@ -2,12 +2,10 @@ const Movie = ({ poster, alt, title, year }) => {
   return (
     <li>
       <img src={poster} alt={alt} />
-      <h2>
-        Title: <span>{title}</span>
-      </h2>
-      <p>
-        Year: <span>{year}</span>
-      </p>
+      <section>
+        <h2>{title}</h2>
+        <p>{`(${year})`}</p>
+      </section>
     </li>
   );
 };
