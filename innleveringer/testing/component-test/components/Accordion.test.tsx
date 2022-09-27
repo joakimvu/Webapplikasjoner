aea845f9c1277c8d783e8693488b332ba0824a9c
joakimvu <joakim.vu@hiof.no>
2022-09-27 14:23:53 +0200

    Moved Accordion component to components folder

---
+++ innleveringer/testing/component-test/components/Accordion.test.tsx
@@ -0,0 +1,33 @@
+import { beforeEach, describe, expect, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Accordion from "../components/Accordion";
+import "@testing-library/jest-dom";
+import React from "react";
+
+// tutorial from
+// https://www.eternaldev.com/blog/testing-a-react-application-with-vitest/
+
+describe("Accordion", () => {
+  beforeEach(() => {
+    render(
+      <Accordion title="Testing">
+        <h4>Content</h4>
+      </Accordion>
+    );
+  });
+
+  test("should show title all the time", () => {
+    expect(screen.getByText(/Testing/i)).toBeInTheDocument();
+  });
+
+  test("should not show the content at the start", () => {
+    expect(screen.queryByText(/Content/i)).not.toBeInTheDocument();
+  });
+
+  test("should show the content on accordion click", async () => {
+    const title = screen.getByText(/Show/i);
+    fireEvent.click(title);
+
+    expect(await screen.findByText(/Content/i)).toBeInTheDocument();
+  });
+});
