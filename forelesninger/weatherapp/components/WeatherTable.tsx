356b7818c24b14d1fefc3b67c7d7e7283ed70cd8
joakimvu <joakim.vu@hiof.no>
2022-10-11 19:55:33 +0200

    Custom Hook

--- forelesninger/weatherapp/components/WeatherTable.tsx
+++ forelesninger/weatherapp/components/WeatherTable.tsx
@@ -1,18 +1,37 @@
 import React from 'react'
+import { data as weatherData } from '../data'
+import { Place } from '../types'
 
-export default function WeatherTable() {
+export default function WeatherTable({ currentLocation }: Place) {
   return (
     <section className="weather-table">
       <h2>Oversikt</h2>
       <ul>
         <li>
           <div>
-            <span>Plass</span>
-            <span>Vær</span>
-            <span>Temperatur</span>
+            <span>
+              <h3>Plass</h3>
+            </span>
+            <span>
+              <h3>Vær</h3>
+            </span>
+            <span>
+              <h3>Temperatur</h3>
+            </span>
           </div>
-          <button type="button">Velg Sted</button>
         </li>
+        {weatherData?.map((data) => (
+          <li key={data?.id}>
+            <div>
+              <span>{data.place}</span>
+              <span>{data.weather}</span>
+              <span>{data.temperature}</span>
+            </div>
+            {currentLocation !== data.place.toLowerCase() ? (
+              <button type="button">Velg Sted</button>
+            ) : null}
+          </li>
+        ))}
       </ul>
     </section>
   )
--- forelesninger/weatherapp/pages/index.tsx
+++ forelesninger/weatherapp/pages/index.tsx
@@ -4,14 +4,22 @@ import StaticText from '../components/StaticText'
 import Title from '../components/Title'
 import WeatherDropdown from '../components/WeatherDropdown'
 import WeatherTable from '../components/WeatherTable'
+import { data } from '../data'
+import useWeather from '../hooks/useWeather'
 
 const Home: NextPage = () => {
+  const { weatherData, currentLocation, handleUpdateWeather } = useWeather(data)
+
   return (
     <>
       <Title title={'Velkommen til oversikten over været'} />
       <StaticText />
-      <WeatherDropdown />
-      <WeatherTable />
+      <WeatherDropdown
+        weatherData={weatherData}
+        currentLocation={currentLocation}
+        handleUpdateWeather={handleUpdateWeather}
+      />
+      <WeatherTable currentLocation={currentLocation} />
     </>
   )
 }
