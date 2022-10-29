292fde4b04435da6922eed698aa198aa6fb46aea
joakimvu <joakim.vu@hiof.no>
2022-10-29 19:01:49 +0200

    created MVC for feeds

--- next-prisma-lection-example/pages/api/feeds/[url].ts
+++ next-prisma-lection-example/pages/api/feeds/[url].ts
@@ -1,19 +1,19 @@
-// import * as feedsController from '@/features/feeds/feeds.controller'
+import * as feedsController from '../../../features/feeds/feeds.controller'
 
-// export default async function handler(req, res) {
-//   const { method } = req
+export default async function handler(req, res) {
+  const { method } = req
 
-//   switch (method?.toLowerCase()) {
-//     case 'get':
-//       await feedsController.getFeedByUrl(req, res)
-//       break
-//     case 'delete':
-//       await feedsController.removeFeedbyUrl(req, res)
-//       break
-//     case 'put':
-//       await feedsController.updateFeedbyUrl(req, res)
-//       break
-//     default:
-//       res.status(405).end()
-//   }
-// }
+  switch (method?.toLowerCase()) {
+    case 'get':
+      await feedsController.getFeedByUrl(req, res)
+      break
+    case 'delete':
+      await feedsController.removeFeedbyUrl(req, res)
+      break
+    case 'put':
+      await feedsController.updateFeedbyUrl(req, res)
+      break
+    default:
+      res.status(405).end()
+  }
+}
--- next-prisma-lection-example/pages/api/users/index.ts
+++ next-prisma-lection-example/pages/api/users/index.ts
@@ -4,14 +4,14 @@ export default async function handler(req, res) {
   const { method } = req
 
 switch (method?.toLowerCase()) {
+  case 'post':
+    // kaller på kontrolleren som brukes til å lage ny bruker
+    await userController.createUser(req, res)
+    break
     // case 'get':
     //   // kaller på kontrolleren som brukes til å hente alle brukere
     //   await userController.listAllUsers(req, res)
     //   break
-    case 'post':
-      // kaller på kontrolleren som brukes til å lage ny bruker
-      await userController.createUser(req, res)
-      break
     default:
       // gir 405 Method Not Allowed hvis brukeren prøver på noe annet 
       // enn POST
