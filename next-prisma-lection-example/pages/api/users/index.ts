59caf9267e42737f6bff6386ab343e4e93cccf9c
joakimvu <joakim.vu@hiof.no>
2022-10-29 16:22:54 +0200

    Update index.ts

--- next-prisma-lection-example/pages/api/users/index.ts
+++ next-prisma-lection-example/pages/api/users/index.ts
@@ -1,20 +1,20 @@
 import * as userController from "../../../features/users/users.controller"
 
 export default async function handler(req, res) {
-  const {method } = req
+  const { method } = req
 
-  switch (method?.toLowerCase()) {
-    case "post":
-    // kaller på kontrolleren som brukes til å lage ny feed
-    await userController.createUser(req, res)
-    break
-    // case "get":
-    // // kaller på kontrolleren som brukes til å hente alle feeds
-    // await userController.listFeeds(req, res)
-    // break
+switch (method?.toLowerCase()) {
+    // case 'get':
+    //   // kaller på kontrolleren som brukes til å hente alle brukere
+    //   await userController.listAllUsers(req, res)
+    //   break
+    case 'post':
+      // kaller på kontrolleren som brukes til å lage ny bruker
+      await userController.createUser(req, res)
+      break
     default:
-    // git 405 Method Not Allowed hvis brukeren prøver på noe annet
-    // enn GET eller POST
-    res.status(405).end()
+      // gir 405 Method Not Allowed hvis brukeren prøver på noe annet 
+      // enn POST
+      res.status(405).end()
   }
 }
